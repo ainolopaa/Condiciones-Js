ecc9a6569fc9982a666c3283ee7712f52148f199
DanielaApolonia. <danielaapoloniaz@gmail.com>
2025-09-05 20:36:21 -0400

    Add files via upload

---
+++ Condiciones_js/Problema2/assets/Js/index.js
@@ -0,0 +1,15 @@
+
+
+function calcular() {
+  let s1 = parseInt(document.getElementById("sticker1").value) || 0;
+  let s2 = parseInt(document.getElementById("sticker2").value) || 0;
+  let s3 = parseInt(document.getElementById("sticker3").value) || 0;
+
+  let total = s1 + s2 + s3;
+
+  if (total <= 10) {
+    document.getElementById("resultado").innerText = "Llevas " + total + " stickers";
+  } else {
+    document.getElementById("resultado").innerText = "Llevas demasiados stickers";
+  }
+}
